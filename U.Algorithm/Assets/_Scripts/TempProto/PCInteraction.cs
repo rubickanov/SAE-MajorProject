7bc2be435108ed22e021ee8eb22db2afd67bae4e
rubickanov <rubickanov@gmai.com>
2024-06-16 13:41:54 +0400

    Added TMP and made pc interaction prototype

---
+++ U.Algorithm/Assets/_Scripts/TempProto/PCInteraction.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+namespace ALG
+{
+    public class PCInteraction : MonoBehaviour
+    {
+        public GameObject osUI;
+        public GameObject interactPopUp;
+        public bool isActive = false;
+        public Transform pc;
+        public float distanceToInteract;
+
+        private void Update()
+        {
+            if ((transform.position - pc.transform.position).magnitude < distanceToInteract)
+            {
+                if (!isActive)
+                {
+                    // pop up and can be interacted
+                    isActive = true;
+                    interactPopUp.SetActive(true);
+                }
+            }
+            else
+            {
+                if (isActive)
+                {
+                    // pop up and can be interacted
+                    isActive = false;
+                    interactPopUp.SetActive(false);
+                }
+            }
+
+            if (isActive)
+            {
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    osUI.SetActive(true);
+                }
+            }
+        }
+    }
+}
